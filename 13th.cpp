5de234164c8cd38d35dd2ec257eb69d3ce3d8480
akshit7rawat <93077890+akshit7rawat@users.noreply.github.com>
2021-10-24 16:25:19 +0530

    Add files via upload

---
+++ 13th.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int marks[] ={38 , 48 , 58 , 68};
+int mathsmarks[] = {60 , 78 , 20 , 67};
+cout<<"these are marks"<<endl;
+
+    cout<<marks[0]<<endl;
+    cout<<marks[1]<<endl;
+    cout<<marks[2]<<endl;
+    cout<<marks[3]<<endl;
+
+    cout<<"these are mathsmarks"<<endl;
+
+    cout<<mathsmarks[0]<<endl;
+    cout<<mathsmarks[1]<<endl;
+    cout<<mathsmarks[2]<<endl;
+    cout<<mathsmarks[3]<<endl;
+    
+    return 0;
+}
